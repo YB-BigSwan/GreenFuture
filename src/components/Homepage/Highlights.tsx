2559424f7b66a29a9d88cc22e2f8f7866ec5ea8d
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-22 21:56:15 +0200

    Started work on highlight section template

---
+++ src/components/Homepage/Highlights.tsx
@@ -0,0 +1,105 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+const Highlights = () => {
+  return (
+    <div className="highlights-wrapper">
+      <div className="highlights-container">
+        <Card className="highlight-card">
+          <CardActionArea>
+            <CardMedia
+              className="card-img"
+              component="img"
+              image="https://plus.unsplash.com/premium_photo-1683134044077-c8af4c752c5f?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <CardContent className="card-content">
+              <Typography className="card-title">Title</Typography>
+              <Typography className="card-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+                obcaecati. Odio possimus dolores earum sint, fugit tempore,
+                accusantium magni ipsa cumque quidem veniam corrupti temporibus.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card className="highlight-card">
+          <CardActionArea>
+            <CardMedia
+              className="card-img"
+              component="img"
+              image="https://plus.unsplash.com/premium_photo-1683134044077-c8af4c752c5f?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <CardContent className="card-content">
+              <Typography className="card-title">Title</Typography>
+              <Typography className="card-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+                obcaecati. Odio possimus dolores earum sint, fugit tempore,
+                accusantium magni ipsa cumque quidem veniam corrupti temporibus.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card className="highlight-card">
+          <CardActionArea>
+            <CardMedia
+              className="card-img"
+              component="img"
+              image="https://plus.unsplash.com/premium_photo-1683134044077-c8af4c752c5f?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <CardContent className="card-content">
+              <Typography className="card-title">Title</Typography>
+              <Typography className="card-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+                obcaecati. Odio possimus dolores earum sint, fugit tempore,
+                accusantium magni ipsa cumque quidem veniam corrupti temporibus.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card className="highlight-card">
+          <CardActionArea>
+            <CardMedia
+              className="card-img"
+              component="img"
+              image="https://plus.unsplash.com/premium_photo-1683134044077-c8af4c752c5f?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <CardContent className="card-content">
+              <Typography className="card-title">Title</Typography>
+              <Typography className="card-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+                obcaecati. Odio possimus dolores earum sint, fugit tempore,
+                accusantium magni ipsa cumque quidem veniam corrupti temporibus.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card className="highlight-card">
+          <CardActionArea>
+            <CardMedia
+              className="card-img"
+              component="img"
+              image="https://plus.unsplash.com/premium_photo-1683134044077-c8af4c752c5f?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <CardContent className="card-content">
+              <Typography className="card-title">Title</Typography>
+              <Typography className="card-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+                obcaecati. Odio possimus dolores earum sint, fugit tempore,
+                accusantium magni ipsa cumque quidem veniam corrupti temporibus.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Highlights;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "../components/Homepage/HeroSection";
+import Highlights from "../components/Homepage/Highlights";
 import "../styles/homepage.css";
 
 function Homepage() {
   return (
     <div className="homepage-container">
       <HeroSection />
+      <Highlights />
     </div>
   );
 }
