dbbd49ddb09f2481b5a2607f0b6bfea279cdd358
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-22 21:23:53 +0200

    adds hero section

---
+++ src/components/Homepage/HeroSection.tsx
@@ -0,0 +1,17 @@
+const HeroSection = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1 className="hero-title">GreenFuture</h1>
+        <p className="hero-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          accusantium impedit, nam sit beatae molestias. Mollitia reprehenderit
+          neque explicabo tenetur ipsam. Ea molestias ut quod quia atque minus
+          nisi itaque!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,12 @@
+import HeroSection from "../components/Homepage/HeroSection";
 import "../styles/homepage.css";
 
 function Homepage() {
-  return <div className="homepage-container">Homepage</div>;
+  return (
+    <div className="homepage-container">
+      <HeroSection />
+    </div>
+  );
 }
 
 export default Homepage;
