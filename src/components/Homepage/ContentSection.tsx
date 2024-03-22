163bb01c33cff7791c4a208eeed8fbd9f63558b1
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-23 00:30:12 +0200

    completed homepage skeleton, will work on accessibility and finishing touches later.

---
+++ src/components/Homepage/ContentSection.tsx
@@ -0,0 +1,33 @@
+const ContentSection = () => {
+  return (
+    <div className="content-section-wrapper">
+      <div className="content-section-container">
+        <img
+          src="https://images.unsplash.com/photo-1621451537084-482c73073a0f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="ocean trash"
+          className="content-section-img"
+        />
+        <p className="content-section-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          dolore aliquid quod earum in, nesciunt sunt amet magnam consectetur
+          recusandae possimus nisi alias accusantium voluptate ratione
+          doloremque perferendis cum est.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio,
+          illo dolores consectetur iure natus odio, aspernatur odit quisquam
+          error non corporis cumque aliquam dicta eveniet eligendi asperiores
+          molestias aut?
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          obcaecati quis quas pariatur voluptatum deleniti, asperiores minus
+          nemo culpa, rerum molestias beatae provident vero eius quod molestiae
+          doloremque, voluptas labore?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ContentSection;
