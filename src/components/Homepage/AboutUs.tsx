c00231f628f2f0afe30c8aa1a8b318911072b025
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-23 16:41:37 +0200

    added homepage content and made nav disappear on scrolldown.

--- src/components/Homepage/AboutUs.tsx
+++ src/components/Homepage/AboutUs.tsx
@@ -5,29 +5,34 @@ const AboutUs = () => {
         <div className="about-us-content">
           <h3 className="about-header">Our Mission</h3>
           <p className="about-description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-            tempora nobis ex eveniet, ipsam, ut doloremque tempore accusantium
-            dolore magni commodi modi excepturi iure dolorum unde fuga
-            perferendis atque. Sit.
+            It is our mission to make sustainability education accessible and
+            transformative for students, driving real-world impact. Through our
+            freemium model, institutional partnerships, and corporate training
+            programs, we aim to build a global community driving positive
+            change.
           </p>
 
-          <h3 className="about-header">Our Mission</h3>
+          <h3 className="about-header">Our Vision</h3>
           <p className="about-description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-            tempora nobis ex eveniet, ipsam, ut doloremque tempore accusantium
-            dolore magni commodi modi excepturi iure dolorum unde fuga
-            perferendis atque. Sit.
+            Our vision is to contribute to creating a world where individuals
+            understand their impact, embrace sustainable choices, and actively
+            work towards global solutions, leading to awareness, engagement, and
+            commitment.
           </p>
 
-          <h3 className="about-header">Our Mission</h3>
-          <p className="about-description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-            tempora nobis ex eveniet, ipsam, ut doloremque tempore accusantium
-            dolore magni commodi modi excepturi iure dolorum unde fuga
-            perferendis atque. Sit.
-          </p>
+          <h3 className="about-header">Our Values</h3>
+          <p className="about-description">Our core values are:</p>
+          <ul>
+            <li>Sustainability</li>
+            <li>Equality</li>
+            <li>Empowerment</li>
+            <li>Collaboration</li>
+            <li>Accountability</li>
+          </ul>
 
-          <h3 className="about-header">Our Mission</h3>
+          <h3 className="about-header">
+            Our Environmental, Social, and Governance (ESG) Statement
+          </h3>
           <p className="about-description">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
             tempora nobis ex eveniet, ipsam, ut doloremque tempore accusantium
--- src/components/Homepage/ContentSection.tsx
+++ src/components/Homepage/ContentSection.tsx
@@ -4,22 +4,24 @@ const ContentSection = () => {
       <div className="content-section-container">
         <div className="content-section-img"></div>
         <p className="content-section-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-          dolore aliquid quod earum in, nesciunt sunt amet magnam consectetur
-          recusandae possimus nisi alias accusantium voluptate ratione
-          doloremque perferendis cum est.
+          A stark reminder of how our choices impact the planet.
           <br />
           <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio,
-          illo dolores consectetur iure natus odio, aspernatur odit quisquam
-          error non corporis cumque aliquam dicta eveniet eligendi asperiores
-          molestias aut?
+          This disturbing image highlights the urgent challenge of plastic
+          pollution, a threat to marine ecosystems, biodiversity, and the health
+          of our planet. This crisis directly impacts several Sustainable
+          Development Goals (SDGs), including SDG 14 (Life Below Water) and SDG
+          12 (Responsible Consumption and Production).
           <br />
           <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          obcaecati quis quas pariatur voluptatum deleniti, asperiores minus
-          nemo culpa, rerum molestias beatae provident vero eius quod molestiae
-          doloremque, voluptas labore?
+          We can be agents of change by shifting to sustainable practices,
+          reducing our plastic consumption and championing responsible waste
+          management. We can help create a cleaner and healthier future on our
+          planet.
+          <br />
+          <br />
+          Let's take action for a sustainable future. Visit our website to learn
+          how you can make a difference and support the SDGs.
         </p>
       </div>
     </div>
