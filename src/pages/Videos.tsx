3599a4936c32a7c310dd2db5887ebfc54342f93f
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-23 11:07:49 +0200

    Hotfix on videos.tsx

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,17 +1,16 @@
 // Videos.js
-import React from 'react';
-import YouTube from 'react-youtube';
-import '../styles/videos.css'; // Import your CSS file
+import YouTube from "react-youtube";
+import "../styles/videos.css"; // Import your CSS file
 
 function Videos() {
   // Extract the video ID from the YouTube URL
-  const videoUrl = 'https://www.youtube.com/watch?v=T6K0yzwJxXM';
-  const videoId = videoUrl.split('v=')[1];
+  const videoUrl = "https://www.youtube.com/watch?v=T6K0yzwJxXM";
+  const videoId = videoUrl.split("v=")[1];
 
   // Options for the YouTube player
   const opts = {
-    height: '390',
-    width: '640',
+    height: "390",
+    width: "640",
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 0,
