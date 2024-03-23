b3de785c65cc76dc11813e7b68488cc18e6e1af7
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-23 15:36:15 +0200

    some styling adjustments

--- src/pages/Petitions.tsx
+++ src/pages/Petitions.tsx
@@ -8,7 +8,7 @@ function Petitions() {
         "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Title",
       description:
-        "<b>I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication.</b>Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
+        "I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication. Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
       link: "https://www.change.org/p/let-s-reduce-the-effect-of-climatic-change-we-can-go-green-by-planting-economic-trees?source_location=petitions_browse",
     },
     {
@@ -16,7 +16,7 @@ function Petitions() {
         "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Title",
       description:
-        "<b>I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication.</b>Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
+        "I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication. Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
       link: "https://www.change.org/p/let-s-reduce-the-effect-of-climatic-change-we-can-go-green-by-planting-economic-trees?source_location=petitions_browse",
     },
     {
@@ -24,7 +24,7 @@ function Petitions() {
         "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Title",
       description:
-        "<b>I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication.</b>Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
+        "I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication. Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
       link: "https://www.change.org/p/let-s-reduce-the-effect-of-climatic-change-we-can-go-green-by-planting-economic-trees?source_location=petitions_browse",
     },
     {
@@ -32,7 +32,7 @@ function Petitions() {
         "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Title",
       description:
-        "<b>I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication.</b>Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
+        "I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication. Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
       link: "https://www.change.org/p/let-s-reduce-the-effect-of-climatic-change-we-can-go-green-by-planting-economic-trees?source_location=petitions_browse",
     },
     {
@@ -40,7 +40,7 @@ function Petitions() {
         "https://images.unsplash.com/photo-1532996122724-e3c354a0b15b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "Title",
       description:
-        "<b>I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication.</b>Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
+        "I will just write this here for now. The vote link could just link to a change.org pertition. Then we dont need to handle voter authentication. Consequatur voluptatem culpa obcaecati ad enim harum aspernatur accusantium voluptate pariatur, magni ab nobis libero facilis dolores. Quos qui quidem quisquam cupiditate magni earum est! Et, enim obcaecati?",
       link: "https://www.change.org/p/let-s-reduce-the-effect-of-climatic-change-we-can-go-green-by-planting-economic-trees?source_location=petitions_browse",
     },
   ];
