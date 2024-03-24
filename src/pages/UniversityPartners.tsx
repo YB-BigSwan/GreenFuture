70cc54f7c996b54436622a658655d43d7cefed94
Stephen Swanson <stephenswanson@Stephens-MacBook-Pro.local>
2024-03-24 06:07:54 +0200

    adding images

--- src/pages/UniversityPartners.tsx
+++ src/pages/UniversityPartners.tsx
@@ -18,8 +18,8 @@ function UniversityPartners() {
             </div>
           </div>
           <img
-            src="https://images.unsplash.com/photo-1511632765486-a01980e01a18?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="people standing together"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5IlqWADfJj2li-gWrgwPUmk66xSdO5OaAwg&usqp=CAU"
+            alt="a row of flags"
             className="about-us-img"
           />
 
